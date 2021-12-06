4c5ea978ce7323185f4a98b27b5b8489964a0f93
Jun Siang Cheah <me@jscheah.me>
2021-12-06 19:32:14 +0000

    2021 day 6

---
+++ 2021/day6-2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import collections
+
+
+input = [int(i) for i in open('day6-1.txt').readline().split(',')]
+
+# map of age -> population
+age_population = {}
+for i in range(10):
+    age_population[i] = 0
+
+for age in input:
+    age_population[age] += 1
+
+for day in range(1, 256+1):
+    # Store spawns for after the decrement stage
+    spawning = age_population[0]
+    # Decrement all ages
+    for i in range(1, 9):
+        age_population[i-1], age_population[i] = age_population[i], age_population[i+1]
+    age_population[6] += spawning
+    age_population[8] += spawning
+    print(f'Day {day}:\tPopulation: {sum(age_population.values())}\t{age_population}')
